b820783ca552210e6eb24e2983252f44f81b3546
il_roman <il.roman@ukr.net>
2019-11-27 20:20:08 +0200

    commit

---
+++ lesson6/task2/index.js
@@ -0,0 +1,10 @@
+function getSum(arr) {
+    let result = 0;
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
+    } else {
+        return null;
+    }
+}
