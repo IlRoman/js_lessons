0be0d202c9a166dff75084047004048c3742f0bb
il_roman <il.roman@ukr.net>
2019-11-27 20:15:09 +0200

    commit

--- lesson6/task1/index.js
+++ lesson6/task1/index.js
@@ -1,5 +1,6 @@
 function getArrayBounds(arr) {
-    if (Array.isArray(arr)) {
-        return [arr.length, arr[0], arr[arr.length - 1]];
-    } else return null;
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    return [arr.length, arr[0], arr[arr.length - 1]];
 }
