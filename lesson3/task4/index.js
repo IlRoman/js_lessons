254607da99f852ac5f90c57d3c04c7cc4871845a
il_roman <il.roman@ukr.net>
2019-11-27 11:34:42 +0200

    commit

--- lesson3/task4/index.js
+++ lesson3/task4/index.js
@@ -2,7 +2,7 @@
 // false || true;
 console.log("false || true: ", false || true);
 // false || true || false;
-console.log("false || true || false:  ", false || true || false);
+console.log("false || true || false: ", false || true || false);
 // false || false || false;
 console.log("false || false || false: ", false || false || false);
 
@@ -10,7 +10,7 @@ console.log("false || false || false: ", false || false || false);
 // false && true;
 console.log("false && true: ", false && true);
 // false && true && false;
-console.log("false  && true && false: ", false && true && false);
+console.log("false && true && false: ", false && true && false);
 // true && true && true;
 console.log("true && true && true: ", true && true && true);
 
@@ -18,7 +18,7 @@ console.log("true && true && true: ", true && true && true);
 
 /* || находит первое истинное значение */
 // 'text' || false;
-console.log("'text' || false:", 'text' || false);
+console.log("'text' || false: ", 'text' || false);
 // true || 'text';
 console.log("true || 'text': ", true || 'text');
 // undefined || null || 0 || '' || null;
@@ -31,7 +31,7 @@ console.log("amount: ", amount);
 
 /* && находит первое ложное значение */
 // 'text' && false;
-console.log("'text' && false:", 'text' && false);
+console.log("'text' && false: ", 'text' && false);
 // true && 'text';
 console.log("true && 'text': ", true && 'text');
 // undefined && null && 0 && '' && null;
@@ -41,7 +41,7 @@ console.log("undefined && null && 0 && '' && null: ", undefined && null && 0 && 
 // !true;
 console.log("!true: ", !true);
 // !false;
-console.log("!false:", !false);
+console.log("!false: ", !false);
 // !'text';
 console.log("!'text': ", !'text');
 // !!'text';
