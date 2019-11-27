20676686a8bafa7cc6d9fb14a24aac80c0afe825
il_roman <il.roman@ukr.net>
2019-11-27 19:22:50 +0200

    commit

--- lesson5/hw4/index.js
+++ lesson5/hw4/index.js
@@ -1,9 +1,5 @@
 function sum(from, to) {
-    let result = 0;
-    for (let i = from; i <= to; i++) {
-        result += i;
-    }
-    return result;
+    return from + to;
 }
 
 function compareSums(a, b, c, d) {
