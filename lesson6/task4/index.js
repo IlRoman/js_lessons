1cf5e505509105ea866488870f7355bc208ec981
il_roman <il.roman@ukr.net>
2019-11-27 21:14:03 +0200

    commit

--- lesson6/task4/index.js
+++ lesson6/task4/index.js
@@ -3,7 +3,7 @@ function swap(arr) {
     if (!Array.isArray(arr)) {
         return null;
     } else {
-        result[arr.length] = arr[0];
+        result[arr.length] = result[0];
         result.shift();
     }
     return result;
