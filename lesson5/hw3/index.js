9a80761ed18d14b6a7e82e1753e2ce9cc6ad5cee
il_roman <il.roman@ukr.net>
2019-11-27 18:45:38 +0200

    commit

--- lesson5/hw3/index.js
+++
@@ -1,3 +0,0 @@
-function increaser(a, index) {
-    return (a > index ? a += index : a);
-}
---
+++ lesson5/hw4/index.js
@@ -0,0 +1,10 @@
+function sum(from, to) {
+    let result = 0;
+    for (let i = from; 1 < to; i++) {
+        result += i;
+    }
+}
+
+function compareSums(a, b, c, d) {
+    return (sum(a, b) > sum(c, d) ? true : false);
+}
