28d726dc7582cb1a3674d607043a130897ad5127
il_roman <il.roman@ukr.net>
2019-11-27 13:44:11 +0200

    commit

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 5; i++) {
     console.log('\n=> ' + i + ' <=');
-    for (let j = 0; j <= 5; j++) {
+    for (let j = 1; j <= 5; j++) {
         console.log(i + ' x ' + j + ' = ' + i * j);
     }
 }
