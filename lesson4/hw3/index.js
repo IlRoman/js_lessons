6efa2b3b3e1d2a027cbff5d55046584038a9394e
il_roman <il.roman@ukr.net>
2019-11-27 14:55:16 +0200

    commit

--- lesson4/hw3/index.js
+++ lesson4/hw3/index.js
@@ -4,9 +4,10 @@ for (let i = 0; i <= 1000; i++) {
     if (i % 2 === 0) {
         console.log('Found');
         count += i;
-        if (count * 5 > 5000) {
-            console.log('bigger');
-        }
-        else { console.log('Smaller or equal') }
     }
 }
+
+
+if (count * 5 > 5000) {
+    console.log('Bigger');
+} else { console.log('Smaller or equal') }
