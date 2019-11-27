c481d92fdebe156dd592be62fcbd86cc384c059b
il_roman <il.roman@ukr.net>
2019-11-27 16:42:00 +0200

    commit

--- lesson5/hw2/index.js
+++
@@ -1,8 +0,0 @@
-function getPrimes(a) {
-    for (let i = 2; i <= a; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) continue;
-        }
-        console.log(i);
-    }
-}
