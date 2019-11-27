03aeb9c7db81bb5cef72f538ccef45e8385a455e
il_roman <il.roman@ukr.net>
2019-11-27 21:19:03 +0200

    commit

--- lesson6/task4/index.js
+++ lesson6/task4/index.js
@@ -1,10 +1,4 @@
 function swap(arr) {
-    let result = arr;
-    if (!Array.isArray(result)) {
-        return null;
-    } else {
-        result[result.length] = result[0];
-        result.shift();
-    }
-    return result;
+    let [firstElement, ...rest] = arr;
+    return [...rest, firstElement];
 }
