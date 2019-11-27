5da33f3a74a2b59c4ea336e8e34370dd2c6eaa14
il_roman <il.roman@ukr.net>
2019-11-27 14:32:43 +0200

    commit

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -6,6 +6,5 @@ for (let i = m; i <= n; i++) {
     if (i % 2 === 0 && 1 % 4 !== 0) result += i;
     else if (i % 3 === 0) result -= i;
     else if (i % 4 === 0) result *= i;
-    else if (i % 5 === 0) console.log(i);
-    else { continue };
+    if (i % 5 === 0) console.log(i);
 }
