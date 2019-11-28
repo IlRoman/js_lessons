d8efdd92c94cb1ba3542242cb97774a9afa1a777
il_roman <il.roman@ukr.net>
2019-11-28 17:55:22 +0200

    commit

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -1,5 +1,8 @@
 function getAdults(obj) {
     let arr = [];
-    let x = Object.entries(obj).filter(x => x[1] >= 18);
+    let x = Object.entries(obj).filter(x => x[1] >= 18).map(x => x[0]);
+    arr.push(x);
     return arr;
 }
+
+// console.log(getAdults({ 'John Doe': 19, 'Tom': 17, 'Bob': 18 }));
