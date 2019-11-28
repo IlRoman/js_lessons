2ca7ba981ea14c41d6c47b0338b4a0057dd37c6a
il_roman <il.roman@ukr.net>
2019-11-28 12:03:46 +0200

    commit

--- lesson6/hw7/index.js
+++ lesson6/hw7/index.js
@@ -1,9 +1,5 @@
 function checker(arr) {
     arr.sort((a, b => a - b));
-    return ((arr[0] + arr[arr.length - 1]) > 1000 ? true : false);
+    if (arr[0] + arr[arr.length - 1] > 1000) return true;
+    else return false;
 }
-
-
-
-// Напишите функцию checker(arr), которая будет суммировать макс и мин элементы массив,
-// и возвращать true если сумма больше 1000, и false в другом случае
