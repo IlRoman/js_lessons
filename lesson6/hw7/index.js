da57d40e1ba14574f716fc253d598417dfcef903
il_roman <il.roman@ukr.net>
2019-11-28 12:01:34 +0200

    commit

--- lesson6/hw7/index.js
+++ lesson6/hw7/index.js
@@ -1,6 +1,6 @@
 function checker(arr) {
     arr.sort((a, b => a - b));
-    return (arr[0] + arr[arr.length - 1] > 1000 ? true : false);
+    return ((arr[0] + arr[arr.length - 1]) > 1000 ? true : false);
 }
 
 
