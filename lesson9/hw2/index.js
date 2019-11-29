25083428d5c3534c447c6c75d5ef8758cc2b1d04
il_roman <il.roman@ukr.net>
2019-11-29 12:06:02 +0200

    commit

--- lesson9/hw2/index.js
+++ lesson9/hw2/index.js
@@ -6,27 +6,3 @@ function getPeople(obj) {
         }, [])
         .map(room => room.name);
 }
-
-
-let rooms = {
-    room1: [
-        { name: 'room1 name1' },
-        { name: 'room1 name2' },
-        { name: 'room1 name3' },
-        { name: 'room1 name4' },
-    ],
-    room2: [
-        { name: 'room2 name1' },
-    ],
-
-    room3: [
-        { name: 'room3 name1' },
-        { name: 'room3 name2' },
-        { name: 'room3 name3' },
-    ],
-}
-
-console.log(getPeople(rooms));
-
-// (плоский массив строк ['room1 name1', 'room1 name2', ... 'room2 name1', 'room3 name1', ...]).
-// Пользуйтесь разнообразием методов массивов и объектов. Обычный цикл for для решения этой задачи подходит не лучшим образом.
