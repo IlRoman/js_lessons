7b6c28b6fae39e457278f55e8825322809f26e1a
il_roman <il.roman@ukr.net>
2019-11-29 18:26:14 +0200

    commit

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -1,9 +1,9 @@
 function multiRound(num) {
     let arr = [];
-    arr.push(Math.floor(+num * 100));
-    arr.push(Math.round(+num * 100));
-    arr.push(Math.ceil(+num * 100));
-    arr.push(Math.trunc(+num * 100));
+    arr.push(Math.floor(+num * 100) / 100);
+    arr.push(Math.round(+num * 100) / 100);
+    arr.push(Math.ceil(+num * 100) / 100);
+    arr.push(Math.trunc(+num * 100) / 100);
     arr.push(+num.toFixed(2));
     return arr;
 }
