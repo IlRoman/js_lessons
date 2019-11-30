3e2da8c0a98dc3ded2f242b56e6262dc89dde5a9
il_roman <il.roman@ukr.net>
2019-11-30 17:07:12 +0200

    commit

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -1,8 +1,9 @@
 function sortContacts(arr, dir = true) {
     if (!Array.isArray(arr)) return null;
     if (dir) {
-        return arr.sort((a, b) => { b.name.localeCompare(a.name) });
-    } else {
         return arr.sort((a, b) => { a.name.localeCompare(b.name) });
+    }
+    if (!dir) {
+        return arr.sort((a, b) => { b.name.localeCompare(a.name) });
     }
 }
