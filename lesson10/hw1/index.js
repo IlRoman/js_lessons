ee387d69c256151a9467fbe46cd824fb13ba6101
il_roman <il.roman@ukr.net>
2019-11-30 13:16:05 +0200

    commit

---
+++ lesson10/hw1/index.js
@@ -0,0 +1,8 @@
+function getTotalPrice(arr) {
+    let result = 0;
+    let arr2 = arr.tiFixed(2);
+    for (let i = 0; i < arr2.length; i++) {
+        result += aar2[i];
+    }
+    return '$' + result;
+}
