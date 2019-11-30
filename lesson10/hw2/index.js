acc69f20d9bb4ff8e17aac56de47bcf099c9a849
il_roman <il.roman@ukr.net>
2019-11-30 13:41:04 +0200

    commit

---
+++ lesson10/hw2/index.js
@@ -0,0 +1,8 @@
+function getRandomNumbers(num, start, finish) {
+    let arr = [];
+    let x = 0;
+    for (let i = 0; i < num; i++) {
+        arr.push(Math.random() * (finish - start) + start);
+    }
+    return arr;
+}
