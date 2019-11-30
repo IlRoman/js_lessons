e0f7ac1bea4f5467e1a88652bb3871859a4a8252
il_roman <il.roman@ukr.net>
2019-11-30 14:23:43 +0200

    commit

--- lesson10/hw3/index.js
+++ lesson10/hw3/index.js
@@ -2,10 +2,14 @@ function superRound(num, rounding) {
     num = Number(num);
     roundind = +rounding;
     let arr = [];
-    arr.push(Math.floor(num * rounding) / rounding);
-    arr.push(Math.round(num * rounding) / rounding);
-    arr.push(Math.ceil(num * rounding) / rounding);
-    arr.push(Math.trunc(num * rounding) / rounding);
-    arr.push(num.toFixed(rounding));
+    let x = 1;
+    for (let i = 0; i < rounding; i++) {
+        x += '0';
+    }
+    arr.push(Math.floor(num * x) / x);
+    arr.push(Math.round(num * x) / x);
+    arr.push(Math.ceil(num * x) / x);
+    arr.push(Math.trunc(num * x) / x);
+    arr.push(num.toFixed(x));
     return arr;
 }
