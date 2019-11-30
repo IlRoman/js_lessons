6bc68c73049baf71005ede7f6c466cb6521a8bcf
il_roman <il.roman@ukr.net>
2019-11-30 13:53:03 +0200

    commit

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -2,6 +2,7 @@ function getRandomNumbers(num, start, finish) {
     let arr = [];
     let x = 0;
     if (finish < start) return null;
+    if (start !== Math.trunc(start) || finish !== Math.trunc(finish)) return null;
     for (let i = 0; i < num; i++) {
         arr.push(Math.trunc(Math.random() * (finish - start) + start));
     }
