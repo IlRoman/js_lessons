65e6ef3321142bbf5564198b922b92cddb5fa18c
il_roman <il.roman@ukr.net>
2019-11-30 17:32:30 +0200

    commit

--- lesson11/hw1/index.js
+++ lesson11/hw1/index.js
@@ -1,14 +1,15 @@
-function splitText(str, len = 10) {
+function splitString(str, num = 10) {
     if (typeof str !== 'string') return null;
     let arr = [];
-    let startPosition = 0;
+    let start = 0;
+
     while (true) {
-        let result = str.substr(startPosition, len);
+        let result = str.substr(start, num);
         if (result.length === 0) {
             break;
         }
         arr.push(result[0].toUpperCase() + result.slice(1));
-        startPosition += len;
+        start += num;
     }
-    return arr.join('\n');
+    return arr;
 }
