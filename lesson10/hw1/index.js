457e7a3339acd30ab1ca905597fe7aa9849ddfc7
il_roman <il.roman@ukr.net>
2019-11-30 13:26:03 +0200

    commit

--- lesson10/hw1/index.js
+++ lesson10/hw1/index.js
@@ -1,6 +1,6 @@
 function getTotalPrice(arr) {
     let result = 0;
-    let arr2 = arr.map(x => x.toFixed(2));
+    let arr2 = arr.map(x => Math.floor(x * 100) / 100);
     for (let i = 0; i < arr2.length; i++) {
         result += Number(arr2[i]);
     }
