7606e611c40dc681ecbfb8d32fdc01be856c25ac
il_roman <il.roman@ukr.net>
2019-11-30 13:18:21 +0200

    commit

--- lesson10/hw1/index.js
+++ lesson10/hw1/index.js
@@ -1,6 +1,6 @@
 function getTotalPrice(arr) {
     let result = 0;
-    let arr2 = arr.tiFixed(2);
+    let arr2 = arr.toFixed(2);
     for (let i = 0; i < arr2.length; i++) {
         result += aar2[i];
     }
