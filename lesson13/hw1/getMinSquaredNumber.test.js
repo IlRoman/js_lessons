8d9769d316e15d453521ee6b9a3cb88b74596ca7
il_roman <il.roman@ukr.net>
2019-12-02 18:16:13 +0200

    commit

--- lesson13/hw1/getMinSquaredNumber.test.js
+++ lesson13/hw1/getMinSquaredNumber.test.js
@@ -1,10 +1,5 @@
 import getMinSquaredNumber from './getMinSquaredNumber.js';
 
-it('should get', function () {
-    const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
-    expect(result).toEqual(4);
-});
-
 it('should get1', function () {
     const result = getMinSquaredNumber([]);
     expect(result).toEqual(null);
@@ -13,4 +8,9 @@ it('should get1', function () {
 it('should get2', function () {
     const result = getMinSquaredNumber('абв');
     expect(result).toEqual(null);
+});
+
+it('should get', function () {
+    const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
+    expect(result).toEqual(4);
 });
