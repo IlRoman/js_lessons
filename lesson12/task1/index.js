9bd4d102cf62738d19a5c4545e9e6778a01c2550
il_roman <il.roman@ukr.net>
2019-12-02 13:23:52 +0200

    commit

--- lesson12/task1/index.js
+++ lesson12/task1/index.js
@@ -1,15 +1,21 @@
 function calc(a) {
     let x = a.split(' ');
-    let result = 0;
+    let result = '';
     switch (x[1]) {
         case '+':
             result = Number(+x[0] + +x[2]);
+            break;
         case '-':
             result = Number(+x[0] - +x[2]);
+            break;
         case '*':
             result = Number(+x[0] * +x[2]);
+            break;
         case '/':
             result = Number(+x[0] / +x[2]);
+            break;
     }
     return a + ' = ' + result;
 }
+
+console.log(calc('16 * 2'));
