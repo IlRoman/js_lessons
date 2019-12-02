7bf0a30e9321ccab50dd0e4d022770962975cb67
il_roman <il.roman@ukr.net>
2019-12-02 18:12:22 +0200

    commit

--- lesson13/hw1/getMinSquaredNumber.js
+++ lesson13/hw1/getMinSquaredNumber.js
@@ -1,4 +1,4 @@
-export function fun(arr) {
+export default function fun(arr) {
     if (!Array.isArray(arr) || arr.length === 0) return null;
     let x = arr.map(a => Math.abs(a));
     return Math.pow(Math.min(...x), 2);
--- lesson13/hw1/getMinSquaredNumber.test.js
+++ lesson13/hw1/getMinSquaredNumber.test.js
@@ -1,4 +1,4 @@
-import { fun } from './getMinSquaredNumber.js';
+import fun from './getMinSquaredNumber.js';
 
 it('should get', function () {
     const result = fun([-777, 3, -2, 6, 45, -20]);
