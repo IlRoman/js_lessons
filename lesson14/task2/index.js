4a04f879821dc7ae1e6481d25b3badbacffbe280
il_roman <il.roman@ukr.net>
2019-12-04 12:14:43 +0200

    commit

---
+++ lesson14/task2/index.js
@@ -0,0 +1,22 @@
+export function createMessenger() {
+    let sender = 'Gromcode';
+    let message = 'Just learn it!';
+
+    function sendMessage(name) {
+        console.log(`${name}, ${message} Your ${sender}`);
+    }
+
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(sender2) {
+        sender = sender2;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    }
+}
