d6ddf0b19363598d8e0737f12ec03803308e5eb7
il_roman <il.roman@ukr.net>
2019-12-05 21:55:32 +0200

    commit

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -1,5 +1,5 @@
-export let event = {
-    message: 'Welcome to the party!',
+let event = {
+    message: 'Welcome to the party',
     guests: [
         { name: 'John', age: 18, email: 'example@server.com' },
         { name: 'Tom', age: 17, email: 'example@server.com' },
@@ -9,9 +9,19 @@ export let event = {
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
                 email: email,
-                text: `Dear ${name}! ${this.message}`
+                text: `Dear ${name}! ${this.message}!`
             }))
     },
 }
 
-console.log(event.getInvitations());
+// console.log(event.getInvitations());
+
+// Отобразите предстоящий праздник в виде объекта, который может разослать приглашения всем гостям
+
+// Основные требования:
+// Экспортируйте из файла объект с именем event
+// Объект event должен хранить имя в свойстве message текст приглашения
+// Объект event должен хранить список гостей в свойстве guests в формате { name: 'John', age: 18, email: 'example@server.com' }
+// Метод объекта getInvitations должен вернуть приглашения для всех гостей, достигших 18 лет в формате
+// { email: 'user-email', message: 'Dear John! Welcome to the party!' }, где John - имя, Welcome to the party!
+// - сообщение, которое храниться в поле message
