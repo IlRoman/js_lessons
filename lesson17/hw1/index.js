f24f2c8b38f37471efa45bd7845655798f54b47e
il_roman <il.roman@ukr.net>
2019-12-05 13:44:09 +0200

    commit

--- lesson17/hw1/index.js
+++ lesson17/hw1/index.js
@@ -5,7 +5,6 @@ export let user = {
         return `${this.firstName} ${this.lastName}`;
     },
     setFullName(fullName) {
-        fullName = this.getFullName();
         let x = fullName.split(' ');
         this.firstName = x[0];
         this.lastName = x[1];
