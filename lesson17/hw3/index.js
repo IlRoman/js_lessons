15c379d69ba0d65cc9ef44fc8c241fc4470ed7d0
il_roman <il.roman@ukr.net>
2019-12-05 17:55:22 +0200

    commit

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -1,5 +1,5 @@
 export function bind(func, context, arg1, arg2) {
-    return function (arg1, arg2) {
-        func.call(context, arg1, arg2);
+    return function (arg3, arg4) {
+        func.call(context, arg3, arg4, arg1, arg2);
     }
 }
