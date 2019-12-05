342186eb9a8284455cf35208015def23b33e20d9
il_roman <il.roman@ukr.net>
2019-12-05 11:24:09 +0200

    commit

---
+++ lesson16/hw1/index.js
@@ -0,0 +1,13 @@
+function createArrayOfFunctions(num) {
+    if (num !== +num) return null;
+    let arr = [];
+    if (!num) return arr;
+    for (let i = 0; i < num; i++) {
+        arr.push(function () {
+            return i;
+        })
+    }
+    return arr;
+}
+
+console.log(createArrayOfFunctions(5));
