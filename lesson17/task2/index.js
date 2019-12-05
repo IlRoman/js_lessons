c5299208b9782deba9371b602ebb0e5c1c6bd32d
il_roman <il.roman@ukr.net>
2019-12-05 12:51:52 +0200

    commit

---
+++ lesson17/task2/index.js
@@ -0,0 +1,10 @@
+let callbackPrompt = {
+    message: 'Tell me your number',
+    showPrompt() {
+        const phoneNumber = prompt(this.message);
+        console.log(phoneNumber);
+    },
+    showDeferredPrompt(ms) {
+        setTimeout(this.showPrompt, 2000);
+    },
+}
