ac6a47f717743cb2b414c335efe3e95ff23c0dcf
il_roman <il.roman@ukr.net>
2019-12-05 21:50:39 +0200

    commit

---
+++ lesson18/task1/index.js
@@ -0,0 +1,17 @@
+let event = {
+    message: 'Welcome to the party!',
+    guests: [
+        { name: 'John', age: 18, email: 'example@server.com' },
+        { name: 'Tom', age: 17, email: 'example@server.com' },
+    ],
+    getInvitations() {
+        return this.guests
+            .filter(({ age }) => age >= 18)
+            .map(({ name, email }) => ({
+                email: email,
+                text: `Dear ${name}! ${this.message}`
+            }))
+    },
+}
+
+console.log(event.getInvitations());
