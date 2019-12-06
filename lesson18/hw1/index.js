0701a39d29fac0ed367f641e41d534caa0b5a844
il_roman <il.roman@ukr.net>
2019-12-06 13:38:47 +0200

    commit

--- lesson18/hw1/index.js
+++ lesson18/hw1/index.js
@@ -1,7 +1,6 @@
 export function spy(fun) {
     let spied = function (...args) {
         if (typeof fun !== 'function') return spied.calls.push(args);
-        spied.calls.push(fun);
         return fun.bind(this)();
     }
     spied.calls = [];
