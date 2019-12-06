083dddf7f715e96831239480f34733135ca2fd29
il_roman <il.roman@ukr.net>
2019-12-06 10:38:59 +0200

    commit

---
+++ lesson18/task2/index.js
@@ -0,0 +1,12 @@
+let wallet = {
+    transactions: [2, 3, 4, 5],
+    getMax() {
+        return Math.max(...this.transactions);
+    },
+    getMin() {
+        return Math.min(...this.transactions);
+    },
+}
+
+console.log(wallet.getMax());
+console.log(wallet.getMin());
