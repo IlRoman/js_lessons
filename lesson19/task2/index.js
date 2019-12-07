817e90f3a24b361abfcb9381a7500e044326ab99
il_roman <il.roman@ukr.net>
2019-12-07 13:32:30 +0200

    commit

---
+++ lesson19/task2/index.js
@@ -0,0 +1,11 @@
+function getOwnProps(obj) {
+    let arr = [];
+    for (let prop in obj) {
+        if (typeof obj[prop] !== 'function' && obj.hasOwnProperty(prop)) {
+            arr.push(prop);
+        }
+    }
+    return arr;
+}
+
+// console.log(getOwnProps(ship));
