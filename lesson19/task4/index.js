8531226b0eca46e0eb5e5a01599efbd94809403f
il_roman <il.roman@ukr.net>
2019-12-07 14:43:03 +0200

    commit

---
+++ lesson19/task4/index.js
@@ -0,0 +1,26 @@
+export function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+User.prototype.sayHi = function () {
+    console.log(`Hi, I am ${this.name}`);
+};
+
+User.prototype.requestNewPhoto = function () {
+    console.log(`New photo request was sent for ${this.name}`);
+};
+
+User.prototype.setAge = function (age) {
+    if (age < 0) {
+        return false;
+    }
+    if (age >= 0) {
+        this.age = age;
+        if (this.age >= 25) console.log(`New photo request was sent for ${this.name}`);
+        return age;
+    }
+};
+
+// const user = new User('Tom', 17);
+// console.log(user);
