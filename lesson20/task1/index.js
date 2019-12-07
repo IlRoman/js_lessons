b7550e8f6ef0b48a97371bb24d721201b8d84eb6
il_roman <il.roman@ukr.net>
2019-12-07 15:35:18 +0200

    commit

--- lesson20/task1/index.js
+++ lesson20/task1/index.js
@@ -1,4 +1,4 @@
-export class User {
+class User {
     constructor(name, age) {
         this.name = name;
         this.age = age;
@@ -24,8 +24,8 @@ export class User {
     };
 
     createEmpty() {
-        this.name = '';
-        this.age = '';
+        this.name;
+        this.age;
     };
 }
 
