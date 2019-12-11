f35c5f58ef0e6a8346917b73d1faf83de0241083
il_roman <il.roman@ukr.net>
2019-12-11 21:47:57 +0200

    commit

---
+++ lesson22/task2/index.js
@@ -0,0 +1,56 @@
+const tasks = [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+]
+
+let createTaskBtn = document.querySelector('.create-task-btn');
+
+createTaskBtn.addEventListener('click', function () {
+    let task_input = document.querySelector('.task-input');
+    tasks.push({ text: task_input.value, done: false, });
+    let allLi = document.querySelector('.list');
+    allLi.innerHTML = '';
+    renderListItems(tasks);
+});
+
+const renderListItems = listItems => {
+    const listElem = document.querySelector('.list');
+
+    const listItemsElems = listItems
+        .sort((a, b) => a.done - b.done)
+        .map(({ text, done }) => {
+            const listItemElem = document.createElement('li');
+            listItemElem.classList.add('list__item');
+            if (done) {
+                listItemElem.classList.add('list__item_done');
+            }
+            const checkboxElem = document.createElement('input');
+            checkboxElem.setAttribute('type', 'checkbox');
+            checkboxElem.checked = done;
+            checkboxElem.classList.add('list__item-checkbox');
+            listItemElem.append(checkboxElem, text);
+            return listItemElem;
+        });
+
+    listElem.append(...listItemsElems);
+}
+
+renderListItems(tasks);
+
+let check = document.querySelectorAll('.list__item-checkbox');
+
+for (let i = 0; i < check.length; i++) {
+    check[i].addEventListener('click', function () {
+        for (let i = 0; i < tasks.length; i++) {
+            if (check[i].checked) {
+                tasks[i].done = true;
+            } else tasks[i].done = false;
+            let allLi = document.querySelector('.list');
+            allLi.innerHTML = '';
+            renderListItems(tasks);
+        }
+    })
+}
