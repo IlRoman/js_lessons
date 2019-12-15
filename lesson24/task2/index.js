a1b470b0c91f411856986d0944018966d9a744be
il_roman <il.roman@ukr.net>
2019-12-15 12:31:01 +0200

    commit

--- lesson24/task2/index.js
+++ lesson24/task2/index.js
@@ -2,7 +2,7 @@ export function getGreenwichTime(date) {
     formatter.format(date);
 }
 
-const formatter = new Intl.DateTimeFormat('en', {
+export const formatter = new Intl.DateTimeFormat('en', {
     timeZone: 'UTC',
     hour: '2-digit',
     minute: '2-digit',
