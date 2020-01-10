e2ea5067c375cbb190374a4aa3eeb9b29fd8bac9
il_roman <il.roman@ukr.net>
2020-01-10 15:33:48 +0200

    commit

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,9 +1,15 @@
-export function getLocalStorageData() {
+export const getLocalStorageData = () => {
     return Object.entries(localStorage)
         .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value);
+            } catch (e) {
+                newValue = value;
+            }
             return {
                 ...acc,
-                [key]: JSON.parse(value),
-            }
+                [key]: newValue,
+            };
         }, {});
-}
+};
