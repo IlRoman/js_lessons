9d78a858412e75610e008a57e5ce5fbaf855b190
il_roman <il.roman@ukr.net>
2020-01-13 17:42:52 +0200

    commit

--- lesson28/hw2/index.js
+++ lesson28/hw2/index.js
@@ -1,26 +1,10 @@
 export function maxFibonacci(n) {
-    let arr = [];
-
-    for (let i = 0; i < n; i++) {
-        arr.push(fib(i))
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        if (c > n) return b
+        a = b;
+        b = c;
     }
-
-    let result = 1;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > result && arr[i] < n) result = arr[i];
-    }
-    return result;
 }
-
-function fib(n) {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
-console.log(maxFibonacci(15));
-
-// Найти наибольшее число Фибоначчи, которое не больше заданного
-
-// Основные требования:
-// Экспортируйте из файла ф-цию maxFibonacci, которая принимает аргументом число больше нуля
-// Ф-ция maxFibonacci должна вернуть наибольшее число Фибоначчи, которое не больше заданного в аргументе
-// Пример работы maxFibonacci(15); // 13
