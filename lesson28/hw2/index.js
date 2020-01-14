6bf6f6ed37ef1a4a43d661a8b4f59b8d8e6c8601
il_roman <il.roman@ukr.net>
2020-01-14 09:10:24 +0200

    commit

--- lesson28/hw2/index.js
+++ lesson28/hw2/index.js
@@ -3,11 +3,11 @@ export function maxFibonacci(n) {
     let b = 1;
     for (let i = 3; i <= n; i++) {
         let c = a + b;
-        if (c > n) return b
+        if (c >= n) return b
         a = b;
         b = c;
     }
     return b;
 }
 
-console.log(maxFibonacci(3));
+console.log(maxFibonacci(13));
