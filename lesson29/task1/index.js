3f3377ad4cde04c6a77efe3d589d6cdb482fbfd3
il_roman <il.roman@ukr.net>
2020-01-15 17:15:43 +0200

    commit

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,4 +1,4 @@
-export const addImage = (imgSrc, callback) => {
+export function addImage(imgSrc, callback) {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
     imgElem.src = imgSrc;
@@ -21,5 +21,3 @@ function callback(error) {
         return;
     }
 }
-
-addImage('https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg', callback)
