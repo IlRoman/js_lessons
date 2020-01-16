720ee52fb3f7e9c2819b7075be35187b7e282ac5
il_roman <il.roman@ukr.net>
2020-01-16 14:13:57 +0200

    commit

--- lesson29/task3/index.js
+++ lesson29/task3/index.js
@@ -5,3 +5,16 @@ export const printing = () => {
     setTimeout(function () { console.log(4); }, 0);
     console.log(3);
 };
+
+function one() {
+    for (let i = 0; i < 5000; i++) {
+        console.log(1);
+    }
+}
+
+function two() {
+    console.log(2);
+}
+
+setTimeout(one(), 5000)
+two();
