648742ca9bdc5bd0879951332141b28fb66a3c9f
il_roman <il.roman@ukr.net>
2020-01-20 09:06:17 +0200

    commit

--- lesson30/hw2/index.js
+++ lesson30/hw2/index.js
@@ -6,11 +6,9 @@ const addImage = (imgSrc, callback) => {
     const containerElem = document.querySelector('.page');
     containerElem.append(imgElem);
 
-    callback = (error, data) => {
+    callback = (error) => {
         if (error) {
             console.log(error);
-        } else {
-            console.log(data);
         }
     };
     const onImageLoaded = () => callback(null, { width: 200, height: 100 });
@@ -20,7 +18,7 @@ const addImage = (imgSrc, callback) => {
 
 };
 
-let imgSrc = 'https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg'
+// let imgSrc = 'https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg'
 
 export const addImageV2 = imgSrc => {
     return new Promise((resolve, reject) => {
@@ -35,9 +33,9 @@ export const addImageV2 = imgSrc => {
     });
 };
 
-addImageV2(imgSrc)
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+// addImageV2(imgSrc)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
 
 // Напишите функцию, которая добавит картинку на страницу и вернет размеры этой картинки
 
