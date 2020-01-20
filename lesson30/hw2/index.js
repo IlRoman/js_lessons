75d6070fe53a8ba79ac086c0ca309afb2497f356
il_roman <il.roman@ukr.net>
2020-01-20 11:19:39 +0200

    commit

--- lesson30/hw2/index.js
+++ lesson30/hw2/index.js
@@ -1,18 +1,6 @@
-const addImage = (imgSrc, callback) => {
-    const imgElem = document.createElement("img");
-    imgElem.setAttribute('alt', 'My Photo');
-    imgElem.src = imgSrc;
+import { addImage } from './addImage.js';
 
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
-
-    const onImageLoaded = () => callback(null, { width: 200, height: 100 });
-    imgElem.addEventListener('load', onImageLoaded);
-
-    imgElem.addEventListener('error', () => callback('Image load failed'));
-};
-
-// let imgSrc = 'https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg'
+let imgSrc = 'https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg'
 
 export const addImageV2 = imgSrc => {
     return new Promise((resolve, reject) => {
