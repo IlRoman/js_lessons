3b570a861ccf7288b44e699e22054e7e8f1b73f7
il_roman <il.roman@ukr.net>
2020-01-20 13:51:21 +0200

    commit

---
+++ lesson31/task1/index.js
@@ -0,0 +1,35 @@
+export function requestUserData(userId) {
+    return new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500)
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    userId: 'userid777',
+                    email: 'userid777@example.com',
+                })
+            }, 1000);
+        }
+    })
+}
+
+requestUserData('broken')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+    .finally(() => console.log('finally'));
+
+
+// Напишите функцию, которая принимает id пользователя и устанавливает проимс в определенное состояние
+
+// Основные требования:
+// Файл должен экспортировать функцию под именем requestUserData
+// Сигнатура: requestUserData(userId), где единственный аргумент - строка - идентификатор пользователя
+
+// requestUserData должна вернуть Promise
+// requestUserData должна перевести промис в состояние "rulfilled" через 1000ms c данными в виде объекта
+// { name: 'John', age: 17, userId: 'userid777', email: 'userid777@example.com' }, где 'userid777' был передан ф-ции в аргументе
+// Ф-ция requestUserData должна перевести промис в состояние "rejected" через 500ms, если переданный userId равен 'broken'
